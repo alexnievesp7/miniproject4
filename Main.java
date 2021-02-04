65cdfeb3802b04b659d827d120aae99d8b78c214
alexnievesp7 <alejandro.nieves@patelhs.org>
2021-02-04 14:14:28 +0000

    first method done

--- Main.java
+++ Main.java
@@ -2,20 +2,26 @@
 import java.util.Scanner;
 
 class Main {
-  public static void main(String[] args) 
-  {
+  public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
 
     System.out.println("What is your name?");
     String name = scan.next();
     System.out.println("What is your favorite class?");
-    String favoriteClass = scan.nextInt();
+    String favoriteClass = scan.next();
     System.out.println("Who is the teacher of that class?");
-    String teacherClass = scan.nextInt();
-    
-    System.out.println("Your favorite class is" + favoriteClass + "and " + teacherclass + "teaches that class.");
-  
+    String teacherClass = scan.next();
   }
-  
-  
+
+  static int periodClass(String favoriteClass, String teacherClass) {
+    System.out.println("Your favorite class is" + favoriteClass + "and " + teacherClass + "teaches that class.");
+    if (favoriteClass == "art" && teacherClass == "Flavio") {
+      return 5;
+    }else if (favoriteClass == "programming" && teacherClass == "Gaber") {
+      return 7;
+    } else {
+      return 0;
+    }
+  }
+
 }
